95bcb0e688490662d3cbb13739e39f2bd3dfbc74
GCVazquez <131416697+GCVazquez@users.noreply.github.com>
2023-06-06 18:22:56 -0400

    Add files via upload

--- Player.java
+++ Player.java
@@ -2,6 +2,11 @@ package softwareEngineeringProject;
 import java.util.Scanner;
 
 public class Player {
+	private static int SHIP_SIZE_2 = 2;
+	private static int SHIP_SIZE_3 = 3;
+	private static int SHIP_SIZE_4 = 4;
+	
+	
 	public int points;
 	Board playerBoard = new Board();
 	Board opponentBoard = new Board();
@@ -10,14 +15,16 @@ public class Player {
 		points = 0;
 		}
 	
-	public void setBoard(Scanner input) {
-		int xcoordinate = 0;
-		int ycoordinate = 0;
-		System.out.println("Please enter a valid x coordinate.");
-		xcoordinate = input.nextInt();
-		System.out.println("Please enter a valid y coordinate.");
-		ycoordinate = input.nextInt();
-		playerBoard.setChar(xcoordinate, ycoordinate, 'b');
+	public void SetBoard(Scanner input) {
+		System.out.println("Begin by selecting a starting coordinate for a ship of size 2.");
+		ShipPlacement(input, SHIP_SIZE_2);
+		playerBoard.print();
+		System.out.println("Now let's select a starting coordinate for a ship of size 3.");
+		ShipPlacement(input, SHIP_SIZE_3);
+		playerBoard.print();
+		System.out.println("And finally, let's set a starting coordinate for a ship of size 4.");
+		ShipPlacement(input, SHIP_SIZE_4);
+		playerBoard.print();
 	}
 	
 	public void Attack(Player opponent, Scanner input) {
@@ -25,28 +32,28 @@ public class Player {
 		int xcoordinate = 0;
 		int ycoordinate = 0;
 		System.out.println("Select an x-coordinate for attack:");
-		xcoordinate = CheckCoordinate(xcoordinate, input);
+		xcoordinate = CheckCoordinate(input, xcoordinate);
 		System.out.println("Select a y-coordinate for attack:");
-		ycoordinate = CheckCoordinate(ycoordinate, input);
+		ycoordinate = CheckCoordinate(input, ycoordinate);
 		
-		if (opponent.playerBoard.getChar(xcoordinate, ycoordinate) == 'b') {
+		if (opponent.playerBoard.getChar(ycoordinate, xcoordinate) == 'b') {
 			System.out.println("You hit something!");
-			opponent.playerBoard.setChar(xcoordinate, ycoordinate, 'x');
-			opponentBoard.setChar(xcoordinate, ycoordinate, 'x');
+			opponent.playerBoard.setChar(ycoordinate, xcoordinate, 'x');
+			opponentBoard.setChar(ycoordinate, xcoordinate, 'x');
 			points++;
 		}
-		else if (opponent.playerBoard.getChar(xcoordinate, ycoordinate) == 'x') {
+		else if (opponent.playerBoard.getChar(ycoordinate, xcoordinate) == 'x') {
 			System.out.println("You've already hit a ship here.");
 		}
 		else {
-			opponentBoard.setChar(xcoordinate, ycoordinate, 'o');
+			opponentBoard.setChar(ycoordinate, xcoordinate, 'o');
 			System.out.println("You missed!");
 		}
 		
 		
 	}
 	
-	public int CheckCoordinate(int coordinate, Scanner input) {
+	public int CheckCoordinate(Scanner input, int coordinate) {
 		while (!input.hasNextInt()) {
 			System.out.println("Not a valid coordinate!");
 			System.out.println("Select a number from 1-8");
@@ -63,6 +70,106 @@ public class Player {
 				break;
 		}
 		return coordinate;
+	}
+	
+	public void SetShipHorizontal(int shipSize, int xcoordinate, int ycoordinate) {
+		for (int i = 1; i <= shipSize; i++) {
+			playerBoard.setChar(ycoordinate, xcoordinate, 'b');
+			xcoordinate++;
+		}
+	}
+	
+	public void SetShipVertical(int shipSize, int xcoordinate, int ycoordinate) {
+		for (int i = 1; i <= shipSize; i++) {
+			playerBoard.setChar(ycoordinate, xcoordinate, 'b');
+			ycoordinate++;
+		}
+	}
+	
+	public boolean CheckShipHorizontal(int shipSize, int xcoordinate, int ycoordinate) {
+		int i = 1;
+		while (i <= shipSize) {
+			if (xcoordinate > 8) {
+				return false;
+			}
+			else if (playerBoard.getChar(ycoordinate, xcoordinate) != '~') {
+				System.out.println("A ship is already in placed here!");
+				return false;
+			}
+			else {
+				xcoordinate++;
+				i++;
+			}
+		}
+		
+			return true;
+	}
+	
+	public boolean CheckShipVertical(int shipSize, int xcoordinate, int ycoordinate) {
+		int i = 1;
+		while (i <= shipSize) {
+			if (ycoordinate > 8) {
+				return false;
+			}
+			else if (playerBoard.getChar(ycoordinate, xcoordinate) != '~') {
+				System.out.println("A ship is already in placed here!");
+				return false;
+			}
+			else {
+				ycoordinate++;
+				i++;
+			}
+		}
+		
+			return true;
+	}
+	
+	public boolean AskShipOrientation(Scanner input) {
+		System.out.println("Would you like to place your ship vertically or horizontally? Enter v or h ");
+		while(input.hasNext()) {
+			char keyPress = input.next().charAt(0);
+			System.out.println(keyPress);
+			if (keyPress != 'h' && keyPress != 'v') {
+				System.out.println("Please enter v for vertical placement, or h for horizontal placement.");
+			}
+			else if (keyPress == 'h')
+				return true;
+			else if (keyPress == 'v')
+				return false;
+		}
+		return true;
+	}
+	
+	public void ShipPlacement(Scanner input, int shipSize) {
+		int xcoordinate = 0;
+		int ycoordinate = 0;
+		boolean validPlacement = false;
+			while(validPlacement == false) {
+				System.out.println("Please enter an x-coordinate from 1-8.");
+				xcoordinate = CheckCoordinate(input, xcoordinate);
+				System.out.println("Please enter an y-coordinate from 1-8.");
+				ycoordinate = CheckCoordinate(input, ycoordinate);
+				if (AskShipOrientation(input) == true) {
+					if (CheckShipHorizontal(shipSize, xcoordinate, ycoordinate) == true) {
+						SetShipHorizontal(shipSize, xcoordinate, ycoordinate);
+						validPlacement = true;
+					}
+					else {
+						System.out.println("Ship placement invalid! Try again.");
+					}
+				
+				}
+				else {
+					if (CheckShipVertical(shipSize, xcoordinate, ycoordinate) == true) {
+						SetShipVertical(shipSize, xcoordinate, ycoordinate);
+						validPlacement = true;
+					}
+					else {
+						System.out.println("Ship placement invalid! Try again.");
+					}
+				
+				}
+			}
 	}
 	
 }
--- main.java
+++ main.java
@@ -3,15 +3,17 @@ import java.util.Scanner;
 
 public class main {
 
-	public static int POINTS_FOR_VICTORY = 7; 
+	public static int POINTS_FOR_VICTORY = 9; 
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	        Scanner input = new Scanner(System.in);
 			Player player1 = new Player();
-	        player1.setBoard(input);
+			System.out.println("Player 1, let's position your ships!");
+	        player1.SetBoard(input);
 	        Player player2 = new Player();
-	        player2.setBoard(input);
+	        System.out.println("Player 2, let's position your ships!");
+	        player2.SetBoard(input);
 	        RunGame(player1, player2, input);
 	}
 	
@@ -21,133 +23,36 @@ public class main {
 	}
 	
 	public static void RunGame(Player player, Player opponent, Scanner input) {
+		int turnCounter = 1;
+		int playerIndicator = 1;
         do {
+        System.out.println("Player 1, it's your turn!");
+        System.out.print("Turn: " + turnCounter);
+        System.out.print("  Player: " + playerIndicator);
+        System.out.print("  Points:" + player.points);
         TakeTurn(player, opponent, input);
         if (player.points >= POINTS_FOR_VICTORY) {
-			System.out.println("Player" + "1" + "Wins!");
+			System.out.println("Player" + " 1 " + "Wins!");
 			System.exit(0);
         }
-        else TakeTurn(opponent, player, input);
+        else {
+        	turnCounter++;
+        	playerIndicator++;
+        	System.out.println("Player 2, it's your turn!");
+            System.out.print("Turn: " + turnCounter);
+            System.out.print("  Player: " + playerIndicator);
+            System.out.print("  Points:" + opponent.points);
+        	TakeTurn(opponent, player, input);
+        	turnCounter++;
+        	playerIndicator--;
+        }
         if (opponent.points >= POINTS_FOR_VICTORY) {
-			System.out.println("Player" + "2" + "Wins!");
+			System.out.println("Player" + " 2 " + "Wins!");
 			System.exit(0);
         }
         
         } while (player.points < POINTS_FOR_VICTORY || opponent.points < POINTS_FOR_VICTORY);
 	}
-	
-	
-	
-	public void ship_placement(char[][] board, int size){
-        Scanner coor_input = new Scanner(System.in);
-        System.out.println("Input a row for the head of your " + size + " unit large ship.");
-        int head_row = coor_input.nextInt;
-        System.out.println("Input a column for the head of your " + size + " unit large ship.");
-        int head_col = coor_input.nextInt;
-
-        if (valid_coordinate(board, head_row, head_col) == False){
-            System.out.println("That coordinate is not within the limits of the board, or is ontop of another ship, try again.");
-            ship_placement(board, size);
-            break;
-        }
-
-        int[] head = {head_row, head_col};
-        System.out.println("Your head coordinates are valid, input a tail coordinate that shares either a row or column with your head, it can't be diagonal!");
-        
-        System.out.println("Input a row for the tail of your " + size + " unit large ship.");
-        int tail_row = coor_input.nextInt;
-        System.out.println("Input a column for the tail of your " + size + " unit large ship.");
-        int tail_col = coor_input.nextInt;
-
-        if (valid_coordinate(board, tail_row, tail_col) == False){
-            System.out.println("That coordinate is not within the limits of the board, or is ontop of another ship, try again.");
-            ship_placement(board, size);
-            break;
-        }
-        int[] tail = {tail_row, tail_col};
 
 
-        if (valid_ship(board, size, head, tail) == True){
-
-        }else{
-            System.out.println("Your ship placement is invalid.");
-            ship_placement(board, size);
-            break;
-        }
-
-    }
-	
-	public boolean valid_coordinate(char[][] board, int row, int col){
-        boolean result = true;
-        if (row < 1 || row > 9) {
-            System.out.println("Row is invalid.");
-            result = false;
-        }
-        if (col < 1 || row > 9){
-            System.out.println("Col is invalid.");
-            result = false;
-        }
-
-        if (board[row][col] != '~'){
-            System.out.println("Ships cannot be on top of eachother.");
-            result = false;
-        }
-
-        return result;
-    }
-
-    public boolean valid_ship(char[][] board, int size, int[] head, int[] tail){
-        String aligned = "None";
-        if(head[0] == tail[0]){
-            if(head[1] > tail[1]){
-                aligned = "horizontal h->t";
-            }else if(head[1] < tail[1]){
-                aligned = "horizontal t->h";
-            } else{
-                return false;
-            }
-            } else if(head[1] == tail[1]) {
-                if(head[0] > tail[0]){
-                    aligned = "vertical h->t";
-                }else if (head[0] < tail[0]){
-                    aligned = "vertical t->h";
-                }
-            }
-        
-
-        if (size > 2){
-            mid_pieces =  (size - 2);
-            if (aligned == "horizontal h->t"){
-                while (mid_pieces > 0){
-                    if (valid_coordinate(board, head[0], head[1] + mid_pieces) == false){
-                        return false;
-                    }
-                    mid_pieces--;
-                }
-            } else if (aligned == "horizontal t->h"){
-                while (mid_pieces > 0){
-                    if (valid_coordinate(board, head[0], head[1] - mid_pieces) == false){
-                        return false;
-                    }
-                    mid_pieces--;
-                }
-            } else if (aligned == "vertical h->t"){
-                while (mid_pieces > 0){
-                    if (valid_coordinate(board, head[0]  + mid_pieces, head[1]) == false){
-                        return false;
-                    }
-                    mid_pieces--;
-                }
-            } else if (aligned == "vertical t->h"){
-                while (mid_pieces > 0){
-                    if (valid_coordinate(board, head[0] - mid_pieces, head[1]) == false){
-                        return false;
-                    }
-                    mid_pieces--;
-                }
-            }
-            
-        }
-
-    }
 }
